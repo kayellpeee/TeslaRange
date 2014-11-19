dffbad8e1df4739fcb878a776ec1c055147f4cb4
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-18 18:55:57 -0800

    defines sample interface

--- geolocation.js
+++ geolocation.js
@@ -1,3 +1,27 @@
 navigator.geolocation.getCurrentPosition(function(position){
   console.log(position);
 });
+
+
+// find client location
+
+// find nearest superchargers
+
+// call google api -> find distance + total elevation change along route
+
+
+
+// load page
+// see closest superchargers (in range), based on battery size
+// select charger
+// see distance to charger (incl. elevation change) & corresponding route
+// see several avg energy usage and charge limit
+// i.e.   250kw/h     136 rated range     140 mi to charger     5 mi buffer   (slow or descending)    (55 mph)    (4:03 pm)
+// i.e.   350kw/h     165 rated range     140 mi to charger     5 mi buffer
+// i.e.   450kw/h     205 rated range     140 mi to charger     5 mi buffer   (climbing or high winds or fast)  (80 mph)...
+// input for energy usage || rated range || time of arrival will calculate the rest
+
+// UI
+// button to refresh
+// button to increase size
+// button change display mode (night vs day)
