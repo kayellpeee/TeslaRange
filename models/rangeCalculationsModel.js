37434556eba6be5b60ade78f66da377932f86143
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-25 14:19:12 -0800

    (feat) implements getLocation method - can't test asynchronous call

--- models/rangeCalculationsModel.js
+++ models/rangeCalculationsModel.js
@@ -1,6 +1,21 @@
 rangeCalculations.model = new function(){
   var methods = {};
-  methods.getLocation = function(){};
+  methods.getLocation = function(){
+    var latitude;
+    var longitude;
+    var timestamp;
+    navigator.geolocation.getCurrentPosition(function(position){
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      timestamp = position.timestamp;
+    });
+    return function(){
+      return {
+        coordinates: [latitude, longitude],
+        timestamp: timestamp
+      }
+    }
+  };
   methods.getNearestChargers = function(latitude, longitude){};
   methods.calculateDistance = function(location, destination, options){};
   methods.calculateChargeNeeded = function(distance, options){};
