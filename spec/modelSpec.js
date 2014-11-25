051e115c3ccb37d0b2fa6a9b591e1528ac8c8e4d
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-25 10:53:52 -0800

    (test) tests calculateChargeNeeded on rangeCalculations model

--- spec/modelSpec.js
+++ spec/modelSpec.js
@@ -69,6 +69,43 @@ function testMithril(mockWindow) {
       return calculateDistance(location, destination, options)[1].elevationFrequency === 1000;
     });
 
+    test(function(){
+      // calculateChargeNeeded takes a distance & options (units, battery size, estimated buffer, avg. energy usage)
+      // returns number of rated miles of charge needed to make the trip, as well as options used for calculation
+      var distance = 100;
+      var options = {
+        units: 'mi',
+        batterySize: 85,        // kWh  (kilowatt hour)
+        buffer: 5,              // 5 miles
+        averageEnergy: 300      // Wh/mi  (Watt hours per mile)
+      };
+      test(function(){
+        return Array.isArray(calculateChargeNeeded(distance, options));
+      });
+      test(function(){
+        return typeof calculateChargeNeeded(distance, options)[0] === 'number';
+      });
+      test(function(){
+        // distance of 100 mi at averageEnergy of 300, 5 mi buffer = 105 mi of rated charge
+        // rated charge is calculated assuming an average energy usage of 300 Wh/mi for the trip
+        return typeof calculateChargeNeeded(distance, options)[0] === 105;
+      });
+      test(function(){
+        return typeof calculateChargeNeeded(distance, options)[1] === 'object';
+      });
+      test(function(){
+        return calculateChargeNeeded(distance, options)[1].batterySize === options.batterySize;
+      });
+      test(function(){
+        return calculateChargeNeeded(distance, options)[1].buffer === options.buffer;
+      });
+      test(function(){
+        // should default to 85 kWh battery (defaults are in the options object above)
+        return calculateChargeNeeded(ditance)[1].batterySize === 85;
+      });
+      return calculateChargeNeeded(distance, options)[1].averageEnergy === options.averageEnergy;
+    });
+
   });
 
 };
