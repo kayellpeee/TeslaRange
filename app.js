d2217375e8e4232344733b89c0d7ae15a2dc6960
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-25 12:02:50 -0800

    (feat) adds methods to rangeCalculations model to pass initial tests

---
+++ app.js
@@ -0,0 +1,2 @@
+var rangeCalculations = {};
+var settings = {};
---
+++ models/rangeCalculationsModel.js
@@ -0,0 +1,8 @@
+rangeCalculations.model = new function(){
+  var methods = {};
+  methods.getLocation = function(){};
+  methods.getNearestChargers = function(latitude, longitude){};
+  methods.calculateDistance = function(location, destination, options){};
+  methods.calculateChargeNeeded = function(distance, options){};
+  return methods;
+};
