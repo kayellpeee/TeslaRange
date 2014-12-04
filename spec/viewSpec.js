cee6f6bf294b0607f3d017c6d9972a9e44c3cbb3
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-03 17:12:09 -0800

    (test) tests div rendered by rangeDashboard view

--- spec/viewSpec.js
+++ spec/viewSpec.js
@@ -1,8 +1,25 @@
-function testMithril(mockWindow) {
-  window = m.deps(mockWindow);
+function testMithril(mock) {
+  // test rangeDashboard view
+
+  test(function(){
+    return typeof rangeDashboard.view === 'function';
+  });
+
+  var root = mock.window.document.createElement("div");
+  m.render(root, rangeDashboard.view());
+
+  test(function(){
+    return root.childNodes.length === 1;
+  });
+  test(function(){
+    return root.childNodes[0].tagName === "DIV";
+  });
+  test(function(){
+    return root.childNodes[0].className === "rangeDashboard";
+  });
 
 };
 
-testMithril(mock.window);
+testMithril(mock);
 
 test.print(function(value) {console.log(value)});
