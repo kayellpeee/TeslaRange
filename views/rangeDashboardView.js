e674903291f0a2b9822b1a7e237402f37b71a154
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-04 00:28:57 -0800

    (feat) first tr has proper th s (from rangeDashboard view (RD-V))

--- views/rangeDashboardView.js
+++ views/rangeDashboardView.js
@@ -1,7 +1,12 @@
 rangeDashboard.view = function(){
   return m("div .rangeDashboard", [
     m("table", [
-      m("tr"),
+      m("tr", [
+        m("th", "Average Energy"),
+        m("th", "Estimated Charge Needed"),
+        m("th", "distance"),
+        m("th", "Estimated Buffer")
+        ]),
       m("tr"),
       m("tr"),
       m("tr"),
