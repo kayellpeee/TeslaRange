e0abb29a13fa6a81e390bd9d5075919c65a7193f
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-12 14:51:43 -0800

    (test) tests basic functions on controller

--- spec/controllerSpec.js
+++ spec/controllerSpec.js
@@ -1,9 +1,22 @@
 function testMithril(mock) {
 
+  // test rangeDashboard controller
   test(function(){
-    return typeof rangeDashboard.controller === 'function';
+    return typeof rangeDashboard.controller === 'object';
   });
-
+  test(function(){
+    // should have proper methods
+    return typeof rangeDashboard.controller.displayEnergy === 'function';
+  });
+  test(function(){
+    return typeof rangeDashboard.controller.displayCharge === 'function';
+  });
+  test(function(){
+    return typeof rangeDashboard.controller.displayDistance === 'function';
+  });
+  test(function(){
+    return typeof rangeDashboard.controller.displayBuffer === 'function';
+  });
 }
 
 testMithril(mock);
