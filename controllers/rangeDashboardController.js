82f4ef17698199efda84f057d60c53eafebba4d1
Keenan Lidral-Porter <klp2012@gmail.com>
2014-12-12 18:43:31 -0800

    (feat) adds functions to rangeDashboard controller

---
+++ controllers/rangeDashboardController.js
@@ -0,0 +1,18 @@
+rangeDashboard.controller = (function(){
+  // should make calls / interact with model fns
+  var methods = {};
+  methods.displayEnergy = function(){};
+  methods.displayCharge = function(){};
+  methods.displayDistance = function(){};
+  methods.displayBuffer = function(){};
+
+  // example:
+  //    rangeDashboard.view returns tr with tds with m.prop s generated from rangeDashboard controller
+
+  // needs to render:
+  //        -> avg energy
+  //        -> distance
+  //        -> charge
+  //        -> buffer
+  return methods;
+}());  
